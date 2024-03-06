f3f95bbda924fbe5df24198fdb431c8f1fc4ba61
rossohinpavel <rossohinpavel@yandex.ru>
2024-03-06 16:42:15 +0300

    asc reports

---
+++ app/info/scripts/asc.py
@@ -0,0 +1,7 @@
+def check_overdue():
+    print('check_overdue')
+
+
+REPORTS = {
+    'Проверка на просроченные заказы': check_overdue
+}
--- app/info/views.py
+++ app/info/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .scripts import asc
 
 
 # Create your views here.
 def index(request: HttpRequest):
-    info_type = request.GET.get('type', None) or 'order'
-    return render(request, f'info/{info_type}.html')
+    _type = request.GET.get('type', None) or request.POST.get('type', None) or 'order'
+    match _type:
+        case 'asc':
+            return asc_index(request)
+        case _:
+            return render(request, f'info/{_type}.html')
+
+
+def asc_index(request: HttpRequest):
+    context = {'reports': asc.REPORTS.keys()}
+    return render(request, f'info/asc.html', context=context)
